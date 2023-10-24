a34e7d320459564145f50e14107f3eea41030535
Yeahger1 <andreholton85@gmail.com>
2023-10-24 20:56:24 +0200

    Monster climbing roof and turning ok

--- Assets/HunterScript.cs
+++ Assets/HunterScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class HunterScript : MonoBehaviour
 {
     public float speed;
     public Transform target;
+    public SpriteRenderer spriteRenderer;
     public float minimumDistance;
     public Vector2 hunterPosition;
     public Vector2 lastHunterPosition;
@@ -16,6 +18,8 @@ public class HunterScript : MonoBehaviour
     public float hunterPositionX;
     public float lastHunterPositionX;
     public bool movingRight = true;
+    public bool movingRightLastFrame = true;
+    public bool upsideDown;
     public float checkDirectionTimer;
     public float dirCheck = -0.2f;
     public float obsticleCheck;
@@ -29,57 +33,67 @@ public class HunterScript : MonoBehaviour
     {
         rb2D = GetComponent<Rigidbody2D>();
         collider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
-    {        
-        if (rb2D.position.x - hunterLastFramePositionX > -0.1)
+    {
+        Vector2 direction = target.position - transform.position;
+        if (direction.x < 0)
         {
-            movingRight = true;            
-        }        
-        else if(Mathf.Abs(rb2D.position.x - hunterLastFramePositionX) > dirCheck)
             movingRight = false;
-
+        }
+        else
+        {
+            movingRight = true;
+        }
+        if (movingRight != movingRightLastFrame)
+        {
+            transform.right *= -1f;
+            if (upsideDown)
+                spriteRenderer.flipY = true;
+            else 
+                spriteRenderer.flipY = false;
+        }
+        movingRightLastFrame = movingRight;
+        
         checkPosTimer += Time.deltaTime;
         checkDirectionTimer += Time.deltaTime;
-
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-        RaycastHit2D wallInfo = Physics2D.Raycast(rb2D.position, Vector2.right, (collider.bounds.size.x/2) + 0.1f);
-        if (wallInfo == true)
-        {
-            GravityFlip();
-            Debug.Log(wallInfo.transform.name);
-        }
         
-        //if (checkPosTimer >= 1)
-        //{
-        //    //obsticleCheck = rb2D.position.x - lastHunterPosition.x;
-        //    //if (movingRight && obsticleCheck < 0.003)
-        //    ////(Vector2.Distance(lastHunterPosition, rb2D.position) < 0.005f)            
-        //    //    GravityFlip();            
-        //    //else if (!movingRight && obsticleCheck < -0.1)
-        //    //    GravityFlip();         
-
-            //    checkPosTimer = 0;
-        //}
-        //else if (checkPosTimer >= 0.5)
-        //{
-        //    lastHunterPosition = rb2D.position;            
-        //}
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if (checkPosTimer >= 1)
+        {        
+            if(math.abs(rb2D.position.x - target.position.x) <= 0.2f)
+                GravityFlip();
+            
+            RaycastHit2D wallInfo = Physics2D.Raycast(rb2D.position, Vector2.right, (collider.bounds.size.x) + 0.1f);
+            Debug.DrawRay(rb2D.position, Vector2.right, Color.red, 0.05f);
+            if (wallInfo == true)
+            {
+                GravityFlip();
+                Debug.Log(wallInfo.transform.name);
+            }
+            checkPosTimer = 0;
+        }
         if (checkDirectionTimer >= 0.1f)
         {
             hunterLastFramePositionX = rb2D.position.x;
             checkDirectionTimer = 0;
         }
-
     }
 
     private void GravityFlip()
     {
         rb2D.gravityScale *= -1;
         transform.up = transform.up * -1;
-        //showMe = rb2D.position.x - lastHunterPosition.x;
-        //Debug.Log(showMe);
+        upsideDown = !upsideDown;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {       
+        if (other.gameObject.TryGetComponent<IDamagable>(out IDamagable damagable))
+        {           
+                damagable.Die();            
+        }        
     }
 }
